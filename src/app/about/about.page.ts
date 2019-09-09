ee18ff1e9753a5bfd66d5f1a4cef334e56d568eb
Fredrick Weir <fnewma1@lsu.edu>
2019-09-08 19:05:08 -0500

    initial commit

---
+++ src/app/about/about.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+//import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.page.html',
+  styleUrls: ['./about.page.scss'],
+})
+export class AboutPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    //console.log('ionViewDidLoad AboutPage');
+    //console.log(this.navParams.get('x'));
+  }
+
+}
+
+//added below via Listing
+//ionViewDidLoad()
+  //console.log('ionViewDidLoad AboutPage');
+  //console.log(this.navParams.get('x'));
